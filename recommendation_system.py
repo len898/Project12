a18742a80b5089309cbdb483e56be98059c0e6f1
Lenni <lennart@familie-richter.de>
2022-12-07 22:34:47 -0800

    Finished just need to add testing file now, and add in test code

--- recommendation_system.py
+++ recommendation_system.py
@@ -1,11 +1,18 @@
-def construct_ratings():
+"""
+Project 12
+Author: Lennart Richter
+file_name: recommendation_system.py & output.txt
+Description: A program that reads in files and provides recommendation for given users and literary titles
+"""
+
+def construct_ratings() -> list:
     list_from_file = []
     book_list = set([])
     name_set = set([])
     ratings_list = []
 
     #Open the file and read all the content into a list
-    with open('test.txt', 'r') as f:
+    with open('ratings-small.txt', 'r') as f:
         for line in f:
             list_from_file.append(line.rstrip())
 
@@ -46,7 +53,7 @@ def construct_ratings():
         
     return book_list,user_rating_dict
 
-def averages(books:list, ratings:dict):
+def averages(books:list, ratings:dict) -> list:
     list_of_avgs = []
     for i in range(0, len(books)):
         running_tally = 0
@@ -59,23 +66,26 @@ def averages(books:list, ratings:dict):
     list_of_avgs.sort(reverse=True)
     return list_of_avgs
 
-def similarities(user:str, ratings:dict):
+def similarities(user:str, ratings:dict) -> list:
+    dict_to_use = {}
+    for user_curr in ratings:
+        if user_curr != user:
+            dict_to_use[user_curr] = ratings[user_curr]
     def dot_product(list1, list2):
         total = 0
         for i in range(0,len(list1)):
             total += (list1[i] * list2[i])
         return total
     #List with reviews from the user we're comparing the others to
-    user_to_be_checked = ratings.pop(user)
+    user_to_be_checked = ratings[user]
 
     list_of_similar_scores = []
 
-    for user in ratings:
-        dot_p = dot_product(user_to_be_checked, ratings[user])
-        list_of_similar_scores.append(tuple([dot_p, user]))
+    for user_curr in dict_to_use:
+        dot_p = dot_product(user_to_be_checked, dict_to_use[user_curr])
+        list_of_similar_scores.append(tuple([dot_p, user_curr]))
 
     list_of_similar_scores.sort(reverse=True)
-    print(list_of_similar_scores)
     return list_of_similar_scores        
 
 def recommended(sims:list,books:list,ratings:dict) -> list:
@@ -101,20 +111,46 @@ def recommended(sims:list,books:list,ratings:dict) -> list:
     name1 = sims[0][1]
     name2 = sims[1][1]
     name3 = sims[2][1]
-    list1 = ratings.pop(name1)
-    list2 = ratings.pop(name2)
-    list3 = ratings.pop(name3)
+    list1 = ratings[name1]
+    list2 = ratings[name2]
+    list3 = ratings[name3]
     average_list_vals = create_averages(list1, list2, list3)
-    print(average_list_vals)
     average_list_vals_and_names = []
     for i in range(0, len(average_list_vals)):
-        average_list_vals_and_names.append(tuple([average_list_vals[i], books[i]]))
+        if average_list_vals[i] > 0:
+            average_list_vals_and_names.append(tuple([average_list_vals[i], books[i]]))
+        else:
+            exit
     average_list_vals_and_names.sort(reverse=True)
-    print(average_list_vals_and_names)
+    return(average_list_vals_and_names)
 
+def print_pretty(input:list) -> None:
+    for i in input:
+        print(str(i[1]) + " " + str(i[0]))
 
+def main():
+    books, ratings = construct_ratings()
+    avgs = averages(books, ratings)
+    
+    print("Welcome to the CS131B Book Recommender. Type the word in the")
+    print("left column to do the action on the right.")
+    print("recommend : recommend books for a particular user")
+    print("averages  : output the average ratings of all books in the system")
+    print("quit      : exit the program")
 
-books, ratings = construct_ratings()
-averages(books, ratings)
-sims = similarities('Kalid',ratings)
-recommended(sims,books,ratings)
+    flag = ""
+    while flag != "quit":
+        flag = input("next task? ")
+        if flag == "recommend":
+            user = input("user? ")
+            if user in ratings:
+                sims = similarities(user, ratings)
+                recs = recommended(sims, books, ratings)
+                print_pretty(recs)
+            else:
+                print_pretty(avgs)
+        elif flag == "averages":
+            print_pretty(avgs)
+        print()
+
+main()
